96ee152db0f0cfa469360ff4472377058eacc4ec
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2021-04-16 20:25:26 +0200

    initial commit
    initial commit. I uploaded the files from my PC to GitHub. I haven't looked through them just yet. Expect another few commits today as I will be fixing the code.

---
+++ index.js
@@ -0,0 +1,29 @@
+
+    window.addEventListener('scroll', event => document.querySelector('.navbar').classList [700 < document.scrollingElement.scrollTop ? 'add' : 'remove']
+  ('scroll-color-portfolio')
+  );
+  window.addEventListener('scroll', event => document.querySelector('.navbar').classList [1600 < document.scrollingElement.scrollTop ? 'add' : 'remove']
+  ('scroll-color-strengths')
+  );
+  window.addEventListener('scroll', event => document.querySelector('.strengths-wrapper').classList [1800 < document.scrollingElement.scrollTop ? 'add' : 'remove']
+  ('strengths-wrapper-hide')
+  );
+  window.addEventListener('scroll', event => document.querySelector('.portfolio-wrapper').classList [800 < document.scrollingElement.scrollTop ? 'add' : 'remove']
+  ('portfolio-wrapper-hide')
+  );
+  window.addEventListener('scroll', event => document.querySelector('.navbar').classList [2600 < document.scrollingElement.scrollTop ? 'add' : 'remove']
+  ('scroll-color-contact')
+  );
+  window.addEventListener('scroll', event => document.querySelector('.contact-wrapper').classList [2700 < document.scrollingElement.scrollTop ? 'add' : 'remove']
+  ('contact-wrapper-hide')
+  );
+
+  function quincyscpOpen() {
+    window.open("https://www.quincyscp.ga/")
+  }
+  function quincymcOpen() {
+    window.open("https://www.quincymc.ga/")
+  }
+  function thescuutOpen() {
+    window.open("https://www.thescuut.com/")
+  }
