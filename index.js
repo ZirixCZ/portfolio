d3c317abe6284be0b166c15a3553e842e2f21886
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2021-05-28 19:59:25 +0200

    Delete not needed code

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-  window.addEventListener('scroll', event => document.querySelector('.strengths-wrapper').classList [1400 < document.scrollingElement.scrollTop ? 'add' : 'remove']
-  ('strengths-wrapper-hide')
-  );
-  window.addEventListener('scroll', event => document.querySelector('.portfolio-wrapper').classList [500 < document.scrollingElement.scrollTop ? 'add' : 'remove']
-  ('portfolio-wrapper-hide')
-  );
-  window.addEventListener('scroll', event => document.querySelector('.about-wrapper').classList [2300 < document.scrollingElement.scrollTop ? 'add' : 'remove']
-  ('about-wrapper-hide')
-  );
-
-  
-  function quincyscpOpen() {
-    window.open("https://www.quincyscp.ga/")
-  }
-  function quincymcOpen() {
+function portfOpen1() {
     window.open("https://www.quincymc.ga/")
-  }
-  function thescuutOpen() {
-    window.open("https://www.thescuut.com/")
   }
